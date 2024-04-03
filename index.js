568fb2fa2846b24c3970065765fd69e23cd1bfee
Jeffrey <netteyjeff@gmail.com>
2024-04-03 17:05:35 +0000

    add product database, APIs

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
-}); 
+});
+
+// api to get all products in database
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// api to get single product fromm id
+app.get("/api/product/:id", async (res, req) => {
+  try {
+    const { id } = req.params; //get id from url
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// api to add products to database
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// connecting to mongodb database
+mongoose
+  .connect(
+    "mongodb+srv://nettey-senpai:j9bsKyZd6Yd3y2SX@backenddb.pobosj1.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to database");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
