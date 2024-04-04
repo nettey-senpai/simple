810aa4827c2998936f652171dcbf3c0381f15791
Jeffrey <netteyjeff@gmail.com>
2024-04-04 12:51:56 +0000

    feat: add form input to APIs

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const Product = require("./models/product.model.js");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
+});
+
+// api to add products to database
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // api to get all products in database
@@ -30,16 +41,6 @@ app.get("/api/product/:id", async (req, res) => {
   }
 });
 
-// api to add products to database
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
 // api to update a product in the database
 app.put("/api/product/:id", async (req, res) => {
   try {
@@ -53,7 +54,6 @@ app.put("/api/product/:id", async (req, res) => {
 
     const updatedProduct = await Product.findById(id);
     res.status(200).json(updatedProduct);
-
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -69,8 +69,7 @@ app.delete("/api/product/:id", async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
-    res.status(200).json({message: "Product deleted successfully"})
-
+    res.status(200).json({ message: "Product deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
