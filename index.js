dabc3031e2bc8f163bb133b11cbac804ad41b249
Jeffrey <netteyjeff@gmail.com>
2024-04-04 12:35:39 +0000

    add api to delete a product from the db

--- index.js
+++ index.js
@@ -43,19 +43,36 @@ app.post("/api/products", async (req, res) => {
 // api to update a product in the database
 app.put("/api/product/:id", async (req, res) => {
   try {
-    const {id} = req.params // get id
+    const { id } = req.params; // get id
 
-    const product = await Product.findByIdAndUpdate(id, req.body)
+    const product = await Product.findByIdAndUpdate(id, req.body);
 
     if (!product) {
-      return res.status(404).json({message: "Product not found"})
+      return res.status(404).json({ message: "Product not found" });
     }
 
-    const updatedProduct = await Product.findById(id)
-    res.status(200).json(updatedProduct)
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
 
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete a product in the database
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params; //get  id
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({message: "Product deleted successfully"})
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
